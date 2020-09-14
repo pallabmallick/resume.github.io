3cf49dd0563a220c8990ee885fdf51d188d1fa19
Pallab Mallick <pallab.ashokabuildcon@gmail.com>
2020-09-14 22:36:52 +0530

    my resume

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Resume - Pallab Kumar Mallick </title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Pallab Kumar Mallick</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/pallabm.jpg" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#project">Project</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Pallab
+                        <span class="text-primary">Mallick</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                       Habra, North 24 Pgs, Westbengal · 91+ 9333326377 ·
+                        <a href="mailto:pallab_mallick@yahoo.in">pallab_mallick@yahoo.in</a>
+                    </div>
+                    <p class="lead mb-5">I have 9 years of experienced at Toll industry as a IT Engineer. Has excellent troubleshooting ability and understanding of toll automation devices and some knowledge about different programming language. </p>
+                    <div class="social-icons">
+                    <a href="/cv/Pallab Mallick.pdf">
+                <button class="btn">
+               <i class="fa fa-download"></i> <h4><font color="brown">CV</font></h4>
+               </button>
+               </a>
+                      <a class="social-icon" href="https://api.whatsapp.com/send?phone=917001973508"><i class="fab fa-whatsapp"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Senior IT Officer</h3>
+                            <div class="subheading mb-3">Ashoka Buildcon Ltd</div>
+                            <p>Supervised and performed the installation of new IT Automation equipment & Software, IT Inventory and Asset management, Coordination with Vendor with different Level, All other IT (hardware/software) related work.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary"> March 2018 - Present</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Junior Team Leader</h3>
+                            <div class="subheading mb-3">Rajdeep Infotechno Pvt Ltd</div>
+                            <p>TMS (Toll Management System) Software and Hardware Support to team member, Server & client PC Installation and set policy for Domain users, Supervision new project installation of TMS and guide to team member, Cross Check Project Activity Sheet, WPMR and send it to IT Manager, Hybrid and Dedicated ETC Lane installation.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">March 2017 - March 2018</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Junior IT Officer</h3>
+                            <div class="subheading mb-3">Ashoka Buildcon Ltd</div>
+                            <p>TMS (Toll Management System) Support L1, HTMS(ETC,ECB,VMS,CCTV,ATCC,MET)
+, Performed regular maintenance of Company peripheral network Devices, All Hardware support L3.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">May 2012 - February 2017</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Hardware & Network Support</h3>
+                            <div class="subheading mb-3">PR Net Com</div>
+                            <p>Handling one individual premise (UCO Bank HO) of around 350 computer systems
+,Backup and Restore User files,Installing and maintenance Microsoft Outlook 6.0 Express, Microsoft
+outlook,Core Banking solution software installing and maintaining (Finacle,
+vender Infosys),Installation and configuration of the Anti Virus application for prevention and cure
+the affected system of using the soft tool provided by that application.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">May 2011 - April 2010</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Culcutta University</h3>
+                            <div class="subheading mb-3">Bachelor of Arts</div>
+                            
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary"> Year of passing 2011</span></div>
+                    </div>
+
+
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">DCA (Diploma in Computer Application)</h3>
+                            <div class="subheading mb-3">HTML, CSS, BOOTSTRAP, PHP, PYTHON, JAVASCRIPT, JAVA</div>
+                            <p>Certificate by Swami Vivekanada Instutite</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2019 to 2020</span></div>
+                    </div><br>
+
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">NIHT Kolkata</h3>
+                            <div class="subheading mb-3">A+ N+ CCNA+ MCSA</div>
+                            <p>Certificate</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2010 to 2012</span></div>
+                </div>
+
+                
+                  
+                
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i><span><h4>HTML</span></h4></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i><span><h4>CSS</span></h4></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i><span><h4>Jquery</span></h4></li>
+                        <li class="list-inline-item"><i class="fab fa-bootstrap"></i><span><h4>Bootstrap</span></h4></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i><span><h4>Wordpress</span></h4></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i><span><h4>PHP</span></h4></li>
+                        <li class="list-inline-item"><i class="fab fa-python"></i><span><h4>Python</span></h4></li>
+                        <li class="list-inline-item"><i class="fab fa-java"></i><span><h4>Java</span></h4></li>
+                       
+                        
+                         </ul>
+                    <div class="subheading mb-3">Database</div>
+                    <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fa fa-database"></i> <span><h4>MySQL</span></h4></li>
+
+                    
+
+                    </ul>
+                        
+                        
+                    </ul>
+                    <div class="subheading mb-3">Opearating Systems</div>
+                    <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-windows"></i><span><h4>windows</span></h4></li>
+                    <li class="list-inline-item"><i class="fab fa-linux"></i><span><h4>linux</span></h4></li>
+
+                    </ul>
+
+                     </ul>
+                    <div class="subheading mb-3">Toll Automation</div>
+                    <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><span><h4>AVCC</span></h4></li>
+                    <li class="list-inline-item"><span><h4>RFID</span></h4></li>
+                     <li class="list-inline-item"><span><h4>atcc</span></h4></li>
+                     <li class="list-inline-item"><span><h4>Boom barrier</span></h4></li>
+                    <li class="list-inline-item"><span><h4>WIM & Staticwebbridge</span></h4></li>
+                
+                    </ul>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Install and Configure Toll Automation Devices
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Install and Maintenance Server and manage Doman Users 
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Web Application Development 
+                        </li>
+                        
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p> I enjoy most of my time being outdoors. I enjoy mountain biking, free climbing, and kayaking.</p>
+                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the back-end web Application world.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="project">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Project</h2>
+                    <ul class="fa-ul mb-0">
+                        
+                        <li>
+                            <span class="fa-li"><i class="fas fa-project-diagram text-warning"></i></span>
+                            Daily Expense Tracke App- Personal Project
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-project-diagram text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                           System Downtime & Stock Management Software- Ashoka Buildcon Ltd
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-project-diagram text-warning"></i></span>
+                            3
+                            <sup>rd</sup>
+                            Manaser to Palwal Toll Project
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-project-diagram text-warning"></i></span>
+                            2
+                            <sup>nd</sup>
+                           Tatanagar Giddibera and baraciru Toll- CKC (IL&FS) Tatanagar
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-project-diagram text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                             Cas Ramgarh Toll Project- Dilip Buildcon Ltd
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-project-diagram text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            HTMS Project - Ashoka Buildcon Ltd 2005
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
